42c9a0d7c170dbcb5216c575f2079a4f8052f020
Ape <andie.ape@gmail.com>
2020-09-20 14:47:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ function dragElement(elmnt) {
 
 
   if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
+
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
+  
     elmnt.onmousedown = dragMouseDown;
   }
 
@@ -25,20 +25,19 @@ function dragElement(elmnt) {
     e.preventDefault();
 
     elmnt.classList.add('active');
-    
-    // get the mouse cursor position at startup:
+ 
     pos3 = e.clientX;
     pos4 = e.clientY;
     
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
+   
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
+   
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
    
@@ -52,7 +51,7 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;  
 
-    // set the element's new position:
+   
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
 
@@ -63,7 +62,7 @@ function dragElement(elmnt) {
 
     checkGoal(leftGoalElem);
     checkGoal(rightGoalElem);
-    // stop moving when mouse button is released:
+   
     document.onmouseup = null;
     document.onmousemove = null;
 
@@ -160,5 +159,5 @@ window.onload = function (e) {
     circles.forEach((x)=>{        
         dragElement(x);
     });
-  //  dragElement(document.getElementById("Group_1"));
+ 
 }
